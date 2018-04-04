49c3b9abe3b8191fb935e4422453fa61aa5a755e
Mark Cooke <mark@creativecookies.net>
2018-04-04 22:27:26 +0930

    * first working commit with example gpx

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tJOG
 the joy of gpx - gpx analysis tools, written in python
+
+Im planning to wrap a lot of functionality into this project, so expect parameters etc. to undergo breaking changes every now and then
+
+```
+usage : tJOG.py [-h] [-ADL] <inputfile>
+    -h : (optional) display help
+    -ADL : (optional) set the origin to the city centre of Adelaide (Australia, South Australia)
+    
+    The output file tJOG_<inputfile> is created, with origin removed/replaced
+    
+    e.g. tJOG.py -ADL test.gpx
+```
--- tJOG.py
+++ tJOG.py
@@ -2,84 +2,107 @@
 # -*- coding: utf-8 -*-
 
 """
-usage : tJOG.py <inputfile> <outputfile>
+usage : tJOG.py [-h] [-ADL] <inputfile>
+    -h : (optional) display help
+    -ADL : (optional) set the origin to the city centre of Adelaide (Australia, South Australia)
+    
+    The output file tJOG_<inputfile> is created, with origin removed/replaced
+    
+    e.g. tJOG.py -ADL test.gpx
 
 https://github.com/micooke/tJOG
 
-Author: Mark Cooke
+@license: MIT
+@copyright: Copyright 2018, Mark Cooke
+@author: Mark Cooke (https://github.com/micooke)
 """
 
 import os
-import sys, getopt
-import gpxpy
-#import mplleaflet
-#import matplotlib.pyplot as plt
-#import numpy as np
-#from scipy.interpolate import interp1d
-#import seawater as sw
-#import re # regular expressions
-from pandas import DataFrame
-
-def gpx_import:
-
+import sys
+import re
 
 def main():
+    url = 'https://github.com/micooke/tJOG'
+    usage = 'usage : tJOG.py [-h] [-ADL] <inputfile>'
     inputfile = ''
     outputfile = ''
-    if len(sys.argv) == 3:
-        inputfile = sys.argv[1]
-        outputfile = sys.argv[2]
+    latOffset = 0.0
+    lonOffset = 0.0
+    
+    # input sanitisation
+    if len(sys.argv) > 1:
+        if sys.argv[1] in {'-h','--help'}:
+            print(usage)
+            return 0
+        elif sys.argv[1] in {'-ADL'}:
+            latOffset = -34.9286600
+            lonOffset = 138.5986300
+            inputfile = sys.argv[2]
+        elif os.path.isfile(sys.argv[1]):
+            inputfile = sys.argv[1]
+        else:
+            print('error: inputfile "'+sys.argv[1]+'" does not exist')
+            return 1
     else:
-        print('usage : tJOG.py <inputfile> <outputfile>')
-        sys.exit(1)
+        print(usage)
+        return 1
 
-    print('Input file is "', inputfile)
-    print('Output file is "', outputfile)
-   
-       # open the source file
-    gpx = gpxpy.parse(open(inputfile))
-    #!head -n 18 example_trk.gpx
-    TPE = gpx.tracks[0].segments[0].points[0].extensions[0]
+    # generate the output filename
+    outputfile = 'tJOG_'+os.path.splitext(inputfile)[0]+'.gpx'
     
-    # gather the TrackPointExtension field names
-    data_fieldnames = ['lat','lon','ele','time','speed']
-    for child in TPE:
-      data_fieldnames.append(re.sub('[^}]+}','',child.tag))
+    print('Input file  :', inputfile)
+    print('Output file :', outputfile)
     
-    # parse all the data
-    data = []
-    segment = gpx.tracks[0].segments[0]
-    for point_idx, point in enumerate(segment.points):
-        point_data = [point.latitude, point.longitude, point.elevation, point.time, segment.get_speed(point_idx)]
-        for child in point.extensions[0]:
-            point_data.append(float(child.text))
-        data.append(point_data)
+    # open the input file and read the contents
+    with open(inputfile, 'r' ) as f:
+        content = f.read()
     
-    # create the DataFrame object
-    df = DataFrame(data, columns=data_fieldnames)
+    ## look for the first lat, lon and assume this is the 'origin'
+    # generate the lat,lon search pattern
+    pattern = re.compile('lat="([^"]+)" lon="([^"]+)"')
     
-    # assume cadence < 100 indicates single foot cadence (i.e. it needs to be doubled)
-    if 'cad' in df.columns:
-      if np.median(df['cad'][0:int(len(df['cad'])/2)]) < 100:
-        df['cad'] *= 2 
+    # find the first lat,lon instance
+    LatLon = pattern.search(content).groups()
     
-    # display some of the table data
-    df.head()
+    # if the string is incomplete, there is no data
+    if len(LatLon) != 2:
+        print('error : No lat,lon information found in the gpx file')
+        return 1
     
-    # remove the origin
-    lon0 = df['lon'][0]
-    lat0 = df['lat'][0]
+    # get the  run origin
+    lat0 = float(LatLon[0])
+    lon0 = float(LatLon[1])
     
-    df['lon'] -= lon0
-    df['lat'] -= lat0
+    # replace each lat,lon instance
+    iterator = pattern.finditer(content)
+    for m in iterator:
+        # get the group
+        LatLon = m.groups()
+        
+        # set the origin to lat,lon = 0,0
+        lat_ = float(LatLon[0]) - lat0 + latOffset
+        lon_ = float(LatLon[1]) - lon0 + lonOffset
+        
+        # format the lat,lon information to 7 decimal places
+        lat_ = "{:.7f}".format(lat_)
+        lon_ = "{:.7f}".format(lon_)
+        
+        # replace the appropriate contents
+        content = content.replace(LatLon[0], lat_)
+        content = content.replace(LatLon[1], lon_)
     
-    # display more data
-    df.head()
+    # replace the creator details
+    s = re.search('creator="([^"]+)"', content).groups()
+    content = content.replace(s[0], url)
+     
+    # write the content to the output file
+    with open(outputfile, 'w' ) as f:
+        f.write(content)
     
-    # save to the output file
-    
+    # normal program return
     return 0
 
-
 if __name__=="__main__":
-    sys.exit(main())
+    result = main()
+    if result > 0:
+        sys.exit(result)
